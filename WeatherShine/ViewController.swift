a7c6fdfb3c53dd4a4617ab681961c8b6d281c631
LegSho <igor_tilt@yahoo.com>
2018-02-03 16:02:44 +0100

    Initial Commit

---
+++ WeatherShine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherShine
+//
+//  Created by Igor Tabacki on 2/3/18.
+//  Copyright © 2018 Igor Tabacki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
